e1bbec46db1b2784c76ebef9b9158e1dc38f3042
Dedemasri <masriansyahd@gmail.com>
2024-06-29 20:31:05 +0700

    981

--- navbar.php
+++ navbar.php
@@ -14,7 +14,7 @@
               <li><a class="nav-link scrollto" href="#services">Menu</a></li>
               <li><a class="nav-link scrollto" href="#about">About Us</a></li>
               <li><a class="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-              <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+              <li><a class="nav-link scrollto" href="#contact">Kontak</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
           </nav><!-- .navbar -->
